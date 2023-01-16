f7b29448d7a588c10aa829a697b427eef38a5aca
oussamabenmahmoud <oussama.benmahmoud@peak-soft.de>
2023-01-16 20:49:48 +0100

    Cypress update

--- cypress/e2e/test_suites/TS01_First_Test_Suite.js
+++ cypress/e2e/test_suites/TS01_First_Test_Suite.js
@@ -5,11 +5,6 @@
 
 
 describe("TS01 - First Spec", () => {
-  beforeEach(() => {
-    cy.viewport(Cypress.env("viewPort"));
-    cy.visit("/");
-    cy.initBrowser();
-  });
 
 
   it("TF01 Hello World Test", () => {
